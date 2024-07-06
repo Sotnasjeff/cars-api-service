bebde05b197d8a68d7007eab60f5c5a30e740569
Jefferson Santos <jefferson.pereira-ext@olxbr.com>
2024-07-06 00:16:04 -0300

    feat: Hello World

---
+++ src/main/java/com/example/cars/api/service/CarsApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cars.api.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cars/api/service/CarsApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cars.api.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
